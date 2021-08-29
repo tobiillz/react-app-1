2f921001172a4073d887dc94a2a17405de4dac47
Tobiillz <ayoadeopeoluwa@yahoo.com>
2021-08-29 04:30:14 +0100

    first commit

---
+++ src/sayHello.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Hello(){
+    const sayHello = ()=>{
+        console.log('Hello!!!!');
+      }
+
+    return(
+        <div>
+            <h3>This is the Hello component</h3>
+            <button onClick={sayHello}> Say Hello </button>
+        </div>
+    )
+}
+
+export default Hello
